d6a6ccedac96d2b34377d68aa9ead0ccf8ef3490
zubieta <25016815+zubieta@users.noreply.github.com>
2025-07-18 09:51:33 -0700

    Add kopia to the image

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,19 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Add kopia repos
+rpm --import https://kopia.io/signing-key
+cat <<EOF | tee /etc/yum.repos.d/kopia.repo
+[Kopia]
+name=Kopia
+baseurl=http://packages.kopia.io/rpm/stable/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://kopia.io/signing-key
+EOF
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux kopia
 
 # Use a COPR Example:
 #
